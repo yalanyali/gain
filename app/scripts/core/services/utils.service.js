e8908497445f5f2df1488d34040339b4cfcc2641
Yigitcan Alanyali <y.alanyali@gmail.com>
2021-01-23 16:12:06 +0100

    foo

--- app/scripts/core/services/utils.service.js
+++ app/scripts/core/services/utils.service.js
@@ -296,7 +296,6 @@
 
             return text.toString().toLowerCase()
                 .replace(/\s+/g, '-')
-                .replace(/[^\w\-]+/g, '')
                 .replace(/\-\-+/g, '-')
                 .replace(/^-+/, '')
                 .replace(/-+$/, '')
